f9141c959f22ed563ac9ff4522fddc166acfbe71
mikhail.safonov <mikhail.safonov@simbirsoft.com>
2018-09-10 23:32:44 +0300

    Добавил pull request Максима.

---
+++ FoodDeliveryBot/Dialogs/ProductsDialog.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Recognizers.Text;
+using Microsoft.Bot.Builder.Prompts.Choices;
+using FoodDeliveryBot.Models;
+using Microsoft.Bot.Builder.Core.Extensions;
+using Microsoft.Bot.Schema;
+using System;
+
+namespace FoodDeliveryBot.Dialogs
+{
+	public class ProductsDialog : DialogContainer
+	{
+		public const string Id = "productsDialog";
+		public const string OrderedProductsList = "orderedProductsList";
+		public static ProductsDialog Instance { get; } = new ProductsDialog();
+		private readonly List<Product> Products = new List<Product>() {
+			//todo: Получать продукты из бд. По Id заказа определять продавашку и тут подгрузить его продукты.
+			new Product { Name = "Classic", Desciption = "Classic shavuha", Price = 120m},
+			new Product { Name = "Non classic", Desciption = "Non classic shavuha", Price = 150m},
+			//todo: вынести в константы
+			new Product { Name = "Завершить"},
+			new Product { Name = "Отменить"}
+		};
+		private List<string> MainMenu
+		{
+			get
+			{
+				return Products.Select(x => x.Name).ToList();
+			}
+		}
+		private ProductsDialog() : base(Id)
+		{
+			this.Dialogs.Add(Id, new WaterfallStep[]
+			{
+				async (dc, args, next) =>
+				{
+					if (args is null || !args.ContainsKey(OrderedProductsList))
+					{
+						dc.ActiveDialog.State[OrderedProductsList] = new OrderedProducts();
+					}
+					else
+					{
+						dc.ActiveDialog.State = new Dictionary<string, object>(args);
+					}
+					//todo: переделать на Hero Card
+					await dc.Prompt("choicePrompt", "Выбор продукта", new ChoicePromptOptions
+					{
+						Choices = ChoiceFactory.ToChoices(MainMenu)
+					});
+				},
+				async (dc, args, next) =>
+				{
+					var choice = (FoundChoice)args["Value"];
+					var option = Products[choice.Index];
+					 var cart = (OrderedProducts)dc.ActiveDialog.State[OrderedProductsList];
+					 if (option.Name == "Завершить")
+					{
+						if (cart.Count > 0)
+						{
+							var userState = UserState<UserInfo>.Get(dc.Context);
+							userState.OrderedProducts = cart;
+							//todo: Просто для примера, потом убрать все в статс
+							var total = cart.Sum(x => x.Price);
+							var message = cart.Select(x => $"{x.Name} - {x.Price}");
+							await dc.Context.SendActivity($"Order ID {userState.Order.OrderId}{Environment.NewLine}" +
+								$"{string.Join(Environment.NewLine, message.ToArray())}{Environment.NewLine}{total}");
+							 await dc.End();
+						}
+						else
+						{
+							await dc.Context.SendActivity(
+								"Вы не выбрали ни одного продукта. Выбирайте блеать!");
+							await dc.Replace(Id);
+						}
+					}
+					else if (option.Name == "Отменить")
+					{
+						await dc.Context.SendActivity("Вы отменили заказ");
+						 dc.ActiveDialog.State.Clear();
+						await dc.End(new Dictionary<string, object>());
+					}
+					else
+					{
+						cart.Add(option);
+						 var total = cart.Sum(x => x.Price);
+						await dc.Context.SendActivity($"Добавлена {option.Name} (${option.Price:0.00})." +
+							Environment.NewLine + Environment.NewLine +
+							$"Еекущий заказ на сумму ${total:0.00}.");
+						 await dc.Replace(Id, dc.ActiveDialog.State);
+					}
+				},
+				async (dc, args, next) =>
+				{
+					await dc.Replace(Id);
+				},
+			});
+			this.Dialogs.Add("choicePrompt", new ChoicePrompt(Culture.English));
+		}
+		private class OrderedProducts : List<Product> { }
+	}
+}
--- FoodDeliveryBot/EchoBot.cs
+++ FoodDeliveryBot/EchoBot.cs
@@ -1,35 +1,99 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.Bot;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Core.Extensions;
+using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Schema;
+using FoodDeliveryBot.Dialogs;
+using FoodDeliveryBot.Models;
 
 namespace FoodDeliveryBot
 {
-    public class EchoBot : IBot
-    {
-        /// <summary>
-        /// Every Conversation turn for our EchoBot will call this method. In here
-        /// the bot checks the Activty type to verify it's a message, bumps the 
-        /// turn conversation 'Turn' count, and then echoes the users typing
-        /// back to them. 
-        /// </summary>
-        /// <param name="context">Turn scoped context containing all the data needed
-        /// for processing this conversation turn. </param>        
-        public async Task OnTurn(ITurnContext context)
-        {
-            // This bot is only handling Messages
-            if (context.Activity.Type == ActivityTypes.Message)
-            {
-                // Get the conversation state from the turn context
-                var state = context.GetConversationState<EchoState>();
+	public class EchoBot : IBot
+	{
+		private const string MainMenuDialogId = "mainMenu";
+		private DialogSet _dialogs { get; } = ComposeMainDialog();
 
-                // Bump the turn count. 
-                state.TurnCount++;
 
-                // Echo back to the user whatever they typed.
-                await context.SendActivity($"Turn {state.TurnCount}: You sent '{context.Activity.Text}'");
-            }
-        }
-    }
+		public async Task OnTurn(ITurnContext context)
+		{
+			if (context.Activity.Type is ActivityTypes.Message)
+			{
+				var userInfo = UserState<UserInfo>.Get(context);
+				var conversationInfo = ConversationState<ConversationInfo>.Get(context);
+
+				// Establish dialog state from the conversation state.
+				var dc = _dialogs.CreateContext(context, conversationInfo);
+
+				// Continue any current dialog.
+				await dc.Continue();
+
+				// Every turn sends a response, so if no response was sent,
+				// then there no dialog is currently active.
+				if (!context.Responded)
+				{
+					//Если еще нет информации о заказе, надо ее получить
+					if (string.IsNullOrWhiteSpace(userInfo?.Order?.OrderId))
+					{
+						await dc.Begin(OrderDialog.Id);
+					}
+					// Otherwise, start our bot's main dialog.
+					else
+					{
+						await dc.Begin(MainMenuDialogId);
+					}
+				}
+			}
+		}
+
+		private static DialogSet ComposeMainDialog()
+		{
+			var dialogs = new DialogSet();
+
+			dialogs.Add(MainMenuDialogId, new WaterfallStep[]
+			{
+				async (dc, args, next) =>
+				{
+					var menu = new List<string> { "Выбрать продукты", "Посмотреть статистику" , "Отменить заказ" };
+					await dc.Context.SendActivity(MessageFactory.SuggestedActions(menu, "How can I help you?"));
+				},
+				async (dc, args, next) =>
+				{
+					var result = (args["Activity"] as Activity)?.Text?.Trim().ToLowerInvariant();
+					switch (result)
+					{
+						//todo: в кнстанты или nameof
+						case "выбрать продукты":
+							await dc.Begin(ProductsDialog.Id);
+							break;
+						case "посмотреть статистику":
+							//todo: добавить просмотр статистики
+							//await dc.Begin(Stats.Id);
+							break;
+						case "отменить заказ":
+							var userState = UserState<UserInfo>.Get(dc.Context);
+							userState.Order = null;
+							userState.OrderedProducts = null;
+							break;
+						default:
+							await dc.Context.SendActivity("Не понимаю.");
+							await next();
+							break;
+					}
+				},
+				async (dc, args, next) =>
+				{
+					// Show the main menu again.
+					await dc.Replace(MainMenuDialogId);
+				}
+			});
+
+			dialogs.Add(OrderDialog.Id, OrderDialog.Instance);
+			dialogs.Add(ProductsDialog.Id, ProductsDialog.Instance);
+			return dialogs;
+		}
+	}
 }
